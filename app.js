3e46c07586a322b1d6263a32e04297837e95f8c4
Sushant Agrawal <sushantagrawal967@gmail.com>
2025-07-26 17:08:21 +0530

    jsx intro

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
-const heading=React.createElement("h1",{},"hi, how are you");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = (<div className='title'>
+                    <h1>Hi Sushant</h1>
+                    <h2> how are you</h2>
+                    <h3>I am fine </h3>
+                </div>);
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const heading=<h1>Now you have written JSX</h1>;
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
---
+++ dist/intro.7c0ccee6.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "hi, how are you");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+//# sourceMappingURL=intro.7c0ccee6.js.map
