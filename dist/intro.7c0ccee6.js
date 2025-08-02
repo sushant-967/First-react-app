1483f2b35d9b81adbe93479732d2d56ede0f18cd
sushant-967 <83686258+sushant-967@users.noreply.github.com>
2025-08-02 17:33:11 +0530

    Delete dist directory

--- dist/intro.7c0ccee6.js
+++
@@ -1,5 +0,0 @@
-const heading = React.createElement("h1", {}, "hi, how are you");
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
-
-//# sourceMappingURL=intro.7c0ccee6.js.map
